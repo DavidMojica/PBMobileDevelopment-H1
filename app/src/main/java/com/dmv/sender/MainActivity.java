b33050857ea8c2968fff4876db6bd0bfb72e7688
David Mojica <davidmojicav@gmail.com>
2024-02-16 12:24:02 -0500

    Java Init

--- app/src/main/java/com/dmv/sender/MainActivity.java
+++ app/src/main/java/com/dmv/sender/MainActivity.java
@@ -3,12 +3,27 @@ package com.dmv.sender;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_change1, btn_change2, btn_nosend, btn_send;
+    TextView text_screen1;
+    String Tag = "test";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btn_change1 = findViewById(R.id.btn1);
+        btn_change2 = findViewById(R.id.btn2);
+
     }
 }
--- app/src/main/java/com/dmv/sender/pantallados.java
+++ app/src/main/java/com/dmv/sender/pantallados.java
@@ -2,5 +2,5 @@ package com.dmv.sender;
 
 public class pantallados {
 
-    
+
 }
