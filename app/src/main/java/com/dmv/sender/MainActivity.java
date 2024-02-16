538b50f9a22bfa89568218b125a947b3363b3780
David Mojica <davidmojicav@gmail.com>
2024-02-16 13:00:47 -0500

    Backing

--- app/src/main/java/com/dmv/sender/MainActivity.java
+++ app/src/main/java/com/dmv/sender/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     Button btn_change1, btn_change2, btn_nosend, btn_send;
     TextView text_screen1;
     String Tag = "test";
+    static byte minPair = 1, maxPair = 99;
 
 
     @Override
@@ -24,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         btn_change1 = findViewById(R.id.btn1);
         btn_change2 = findViewById(R.id.btn2);
+        btn_nosend = findViewById(R.id.btn_nosend);
+        btn_send = findViewById(R.id.btn_sender);
+        text_screen1 = findViewById(R.id.text_screen1);
 
+        //------------ Listeners --------------//
+
+
+    }
+
+
+    private HashMap<Boolean, Byte> getVerificationNumber() {
+        HashMap<Boolean, Byte> pairs = new HashMap<>();
+        byte truePos = getRandomNumber(0, 2);
+        for (byte i = 0; i < 3; i++){
+            boolean key = (i == truePos) ? true : false;
+            pairs.put(key, getRandomNumber(minPair, maxPair));
+        }
+
+        return pairs;
+    }
+
+    private static byte getRandomNumber(int min, int max){
+        Random rnd = new Random();
+        return (byte) (rnd.nextInt(max - min + 1) + min);
     }
 }
