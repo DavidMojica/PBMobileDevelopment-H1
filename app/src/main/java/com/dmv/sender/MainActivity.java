f69541794bd3d3aa24fe98dc0fcec99709de282c
David Mojica <davidmojicav@gmail.com>
2024-02-16 10:51:16 -0500

    Project setup

---
+++ app/src/main/java/com/dmv/sender/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dmv.sender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
